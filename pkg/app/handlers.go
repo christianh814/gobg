7d0eda2fc368508f8492e43a98ae6442565a56eb
Christian Hernandez <christian@codefresh.io>
2022-08-30 16:53:11 -0700

    going back to blue with PR step
    Signed-off-by: Christian Hernandez <christian@codefresh.io>

--- pkg/app/handlers.go
+++ pkg/app/handlers.go
@@ -19,8 +19,8 @@ const InfoHtml string = "html/info.tmpl"
 func appRoot(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles(IndexHtml))
 	greet := AppSetting{
-		Greeting: "This is pink",
-		Color:    "pink",
+		Greeting: "Version: Blue Square",
+		Color:    "blue",
 	}
 
 	// Display index page from template
