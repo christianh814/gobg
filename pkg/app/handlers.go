da33f827e842a00f44365a96cd16e1e0538a2245
Christian Hernandez <christian@codefresh.io>
2022-08-31 13:36:43 -0700

    updated to blue
    Signed-off-by: Christian Hernandez <christian@codefresh.io>

--- pkg/app/handlers.go
+++ pkg/app/handlers.go
@@ -19,8 +19,8 @@ const InfoHtml string = "html/info.tmpl"
 func appRoot(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles(IndexHtml))
 	greet := AppSetting{
-		Greeting: "Pumpkin Spice Season!",
-		Color:    "orange",
+		Greeting: "Blue Box Version 1",
+		Color:    "blue",
 	}
 
 	// Display index page from template
