542b48e8eb3a60a84e953b291e444e642af2eb5d
Christian Hernandez <christian@codefresh.io>
2022-09-01 10:44:23 -0700

    going pink
    Signed-off-by: Christian Hernandez <christian@codefresh.io>

--- pkg/app/handlers.go
+++ pkg/app/handlers.go
@@ -19,8 +19,8 @@ const InfoHtml string = "html/info.tmpl"
 func appRoot(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles(IndexHtml))
 	greet := AppSetting{
-		Greeting: "Pumpkin",
-		Color:    "orange",
+		Greeting: "Bruh",
+		Color:    "pink",
 	}
 
 	// Display index page from template
