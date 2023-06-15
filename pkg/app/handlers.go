18429ac279d79c46d7e32961a6ecd47751fd928c
Christian Hernandez <christian@chernand.io>
2023-06-15 16:14:02 -0700

    going green
    Signed-off-by: Christian Hernandez <christian@chernand.io>

--- pkg/app/handlers.go
+++ pkg/app/handlers.go
@@ -19,8 +19,8 @@ const InfoHtml string = "html/info.tmpl"
 func appRoot(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles(IndexHtml))
 	greet := AppSetting{
-		Greeting: "Just a Blue Box",
-		Color:    "blue",
+		Greeting: "Just a Green Box",
+		Color:    "green",
 	}
 
 	// Display index page from template
